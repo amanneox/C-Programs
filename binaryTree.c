d02b2f1ee3be93cfe8ffd79496d26e8d6ba33d17
Aman Adhikari <amanadhikari2@gmail.com>
2018-05-24 18:31:20 +0530

    TREE

--- binaryTree.c
+++ binaryTree.c
@@ -16,9 +16,26 @@ void printPreorder(struct node* node)
 {
      if (node == NULL)
           return;
-     printf("%d ", node->data);
+     printf("%3d", node->data);
      printPreorder(node->left);
      printPreorder(node->right);
+}
+void printPostorder(struct node* node){
+  if (node ==NULL) {
+    return;
+  }
+    printPostorder(node->left);
+    printPostorder(node->right);
+    printf("%3d",node->data);
+}
+void printInorder(struct node* node){
+  if(node==NULL){
+    return;
+  }
+  printInorder(node->left);
+  printf("%3d",node->data);
+  printInorder(node->right);
+
 }
 int main(){
   struct node *root=newNode(1);
@@ -26,7 +43,13 @@ int main(){
   root->right= newNode(3);
   root->left->left = newNode(4);
   root->left->right= newNode(5);
-
+  printf("%s\n","PREORDER:");
   printPreorder(root);
+  printf("\n");
+  printf("%s\n","POSTORDER:");
+  printPostorder(root);
+  printf("\n");
+  printf("%s\n","INORDER:");
+  printInorder(root);
   return 0;
 }
